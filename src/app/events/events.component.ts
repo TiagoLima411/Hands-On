d79aaa516551ad300baad012d22c023511be46ac
Tiago <tiago_lima411@hotmail.com>
2018-07-20 18:08:36 -0300

    style(style): style
    somente estilo

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,5 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 import { EventService } from './shared/event.service';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+import { AfService } from '../providers/af.service';
+import { User } from '../providers/user';
+import { VoluntarioService } from './shared/voluntario.service';
+import { Voluntario } from './shared/voluntario';
+import { Event } from './shared/event';
 
 @Component({
   selector: 'app-events',
@@ -8,10 +18,34 @@ import { EventService } from './shared/event.service';
   providers :[EventService]
 })
 export class EventsComponent implements OnInit {
+  @Input() event: Event;
 
-  constructor(private EventService : EventService) { }
+  private voluntario: Voluntario;
+  user: User;
+
+  NgModuleMatCardModule
+  events: Observable<Event[]>;
+
+  constructor(
+    private EventService : EventService, 
+    private db: AngularFireDatabase,
+    public afService: AfService,
+    private voluntarioService: VoluntarioService,
+  ) { }
 
   ngOnInit() {
+    this.events = this.getPages('/events');
+    this.afService.user$.subscribe(user => {
+      this.user = user;
+    });
+  }
+
+  getPages(listPath): Observable<any[]> {
+    return this.db.list(listPath, ref => ref.orderByChild('active').equalTo(Boolean(true))).valueChanges();
+  }
+
+  createVoluntario() {
+    this.voluntarioService.createVoluntario(this.event.key)
   }
 
 }
